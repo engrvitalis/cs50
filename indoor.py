1df73577690b86bd0374f11871ad1e78338cdb35
Nnamdi <ndlinks@gmail.com>
2024-01-12 20:57:32 +0100

    Added the docstring

---
+++ indoor.py
@@ -0,0 +1,9 @@
+"""
+Project title:	Indoor
+Description:	This program prompts the user for input and then outputs that same input
+in lowercase with punctuation and whitespace outputted unchanged.
+Programmer:	Nnamdi Vitalis Ewuzie
+Date:		12/02/2024.
+
+"""
+
