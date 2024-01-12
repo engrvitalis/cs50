9bcea1b7330e016c208ebc24382fa8224a70070d
Nnamdi <ndlinks@gmail.com>
2024-01-12 23:53:50 +0100

    initial commit

---
+++ faces.py
@@ -0,0 +1,7 @@
+"""
+Description: This function accepts a str as input and returns that same input with any :) converted to 🙂 (otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face). All other text returned unchanged.
+
+Programmer: Nnamdi Vitalis Ewuzie
+
+"""
+
