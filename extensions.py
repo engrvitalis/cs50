ae25426b28d7ef139642206299febe518dec877d
Nnamdi <ndlinks@gmail.com>
2024-01-16 21:12:29 +0100

    initial commit

---
+++ extensions.py
@@ -0,0 +1,30 @@
+"""
+This project implements a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, case-insensitively, in any of these suffixes:
+
+"""
+
+def main():
+
+	#prompt the user for the name of a file
+	f = input("Enter file name: ")
+	e = f[f.find(".")+1:]
+
+	#outputs that file’s media type
+	match e:
+		case "gif":
+			print("image/gif")
+		case "jpg" | "jpeg":
+			print("image/jpeg")
+		case "png":
+			print("images/png")
+		case "pdf":
+			print("application/pdf")
+		case "txt":
+			print("text/plain")
+		case "zip":
+			print("application/zip")
+		case _:
+			print("application/octet-stream")
+
+
+main()
