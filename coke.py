9a1bdd23302c326bafc14a7d403b39b64ad34355
Nnamdi <ndlinks@gmail.com>
2024-01-20 12:54:05 +0100

    implementation complete

--- coke.py
+++ coke.py
@@ -3,9 +3,19 @@ This program prompts the user to insert a coin, one at a time, each time informi
 """
 
 def main():
-	pass
+	amount_due = 50
+	print(f'Amount Due: {amount_due}')
 
-def calculate(n):
-	pass
+	while True:
+		coin = int(input("Insert coin: "))
+		amount_due = calculate(amount_due, coin)
+		if amount_due > 0:
+			print(f'Amount Due: {amount_due}')
+		else:
+			print(f'Change Owed: {abs(amount_due)}')
+			break
+
+def calculate(amount_due, coin):
+	return amount_due - coin
 
 main()
