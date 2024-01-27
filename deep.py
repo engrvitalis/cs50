42112f71fa47ee763858c7088258db4ad2b2b2fd
Nnamdi <ndlinks@gmail.com>
2024-01-27 11:31:32 +0100

    Debug: corrected fourty-two to forty-two in line 11

--- deep.py
+++ deep.py
@@ -1,11 +1,14 @@
 """
-This program implements a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+This program implements a program that prompts the user for the 
+answer to the Great Question of Life, the Universe and Everything, 
+outputting Yes if the user inputs 42 or (case-insensitively) 
+forty-two or forty two. Otherwise output No.
 
 """
 def main():
-    ans = input("What is the answer to the great question of life: ").lower()
+    ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
     
-    if ans == "42" or ans == "fourty-two" or ans == "forty two":
+    if ans in ["42", "forty-two", "forty two"]:
         print("Yes")
     else:
         print("No")
