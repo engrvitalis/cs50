d1d3f99aca51b32932a8d08fce98960ea2f945f7
Nnamdi <ndlinks@gmail.com>
2024-01-27 22:27:52 +0100

    Debug

--- extensions.py
+++ extensions.py
@@ -1,5 +1,7 @@
 """
-This project implements a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, case-insensitively, in any of these suffixes:
+This project implements a program that prompts the user for the 
+name of a file and then outputs that file’s media type if the 
+file’s name ends, case-insensitively, in any of these suffixes:
 
 """
 
@@ -7,7 +9,12 @@ def main():
 
 	#prompt the user for the name of a file
 	f = input("Enter file name: ").lower().strip()
-	e = f[-3:]
+	i = len(f) - 1
+	while i >= 0:
+		if f[i] == '.':
+			e = f[i+1:]
+			break
+		i -= 1
 
 	#outputs that file’s media type
 	match e:
