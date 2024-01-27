af83d9e013555d28890d95677b8490a185fa3964
Nnamdi <ndlinks@gmail.com>
2024-01-27 11:42:14 +0100

    Added support for text prefixed or suffixed with space(s)

--- deep.py
+++ deep.py
@@ -6,7 +6,7 @@ forty-two or forty two. Otherwise output No.
 
 """
 def main():
-    ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+    ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
     
     if ans in ["42", "forty-two", "forty two"]:
         print("Yes")
